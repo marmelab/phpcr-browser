8043a247171e8dd4e66de82d41ca29dc82c74a96
Robin Bressan <robin@buddey.net>
2014-04-04 18:59:32 +0200

    Add alphabetical sorter on properties

---
+++ web/assets/js/browser/filters/properties-sorter.js
@@ -0,0 +1,30 @@
+(function(angular, app) {
+  'use strict';
+
+  app.filter('mbPropertiesSorter', function() {
+
+    return function(inputs) {
+      if (inputs) {
+        var alphabeticalSorter = function(a, b) {
+          if (a.name < b.name) {
+            return -1;
+          } else {
+            return 1;
+          }
+        };
+
+        var jcr = inputs.filter(function(a) {
+          return a.name.match('(.+):(.+)');
+        }).sort(alphabeticalSorter);
+
+        var others = inputs.filter(function(a) {
+          return !a.name.match('(.+):(.+)');
+        }).sort(alphabeticalSorter);
+
+        inputs = others.concat(jcr);
+      }
+
+      return inputs;
+    };
+  });
+})(angular, angular.module('browserApp'));
