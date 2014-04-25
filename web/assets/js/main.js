44b54a6a0385d276ac065b4ed9c9fd0c976e18a5
Robin Bressan <robin@buddey.net>
2014-04-25 08:31:03 +0200

    Add requirejs main file

---
+++ web/assets/js/main.js
@@ -0,0 +1,59 @@
+/* global require
+   jshint indent:2
+*/
+(function() {
+  'use strict';
+
+  /**
+   * @see: https://github.com/tnajdek/angular-requirejs-seed
+   */
+  var rootDir = '../../';
+  var vendor = function(relPath, uncompressed) {
+    return rootDir + 'bower_components/' + relPath + (!uncompressed ? '.min' : '');
+  };
+
+  require.config({
+    baseUrl: 'assets/js/browser',
+    paths: {
+      // Vendors
+      angular:            vendor('angular/angular'),
+      jquery:             vendor('jquery/dist/jquery'),
+      bootstrap:          vendor('bootstrap/dist/js/bootstrap'),
+      angularRoute:       vendor('angular-route/angular-route'),
+      angularAnimate:     vendor('angular-animate/angular-animate'),
+      angularJSToaster:   vendor('AngularJS-Toaster/toaster'),
+      angularUIRouter:    vendor('angular-ui-router/release/angular-ui-router'),
+      angularUIKeypress:  vendor('angular-ui-utils/keypress'),
+      lodash:             vendor('lodash/dist/lodash'),
+      restangular:        vendor('restangular/dist/restangular'),
+      talker:             vendor('talker/talker-0.1.0'),
+      angularXEditable:   vendor('angular-xeditable/dist/js/xeditable')
+    },
+    shim: {
+      angular : {'exports' : 'angular'},
+      angularAnimate: ['angular'],
+      angularRoute: ['angular'],
+      angularJSToaster: ['angularAnimate'],
+      angularUIRouter: ['angular'],
+      angularUIKeypress: ['angular'],
+      restangular: ['angular'],
+      talker: ['angular'],
+      angularXEditable: ['angular']
+    },
+    priority: [
+      'angular'
+    ]
+  });
+
+  //http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
+  window.name = 'NG_DEFER_BOOTSTRAP!';
+
+  require([
+    'angular',
+    'app'
+  ], function (angular, app) {
+    angular.element().ready(function () {
+      angular.resumeBootstrap([app.name]);
+    });
+  });
+})();
