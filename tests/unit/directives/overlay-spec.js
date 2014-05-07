54ae6c383e6e0515ce8f6632aa4b2ecc2b2367b8
Robin Bressan <robin@buddey.net>
2014-05-07 15:01:31 +0200

    Add tests for overlay

---
+++ tests/unit/directives/overlay-spec.js
@@ -0,0 +1,59 @@
+/*global define,describe,it,beforeEach,module,inject,expect*/
+/* jshint indent:2 */
+
+define([
+  'angular',
+  'angularMocks',
+  'app',
+  'directives/ui/overlay'
+], function () {
+  'use strict';
+
+  describe('Directive: Overlay', function () {
+    var element,
+        text = 'Awesome text',
+        hideOn = 'hideMeEvent',
+        showOn = 'showOnEvent',
+        $rootScope,
+        $scope,
+        $compile;
+
+    // load the directive's module
+    beforeEach(module('browserApp'));
+
+    beforeEach(inject(function($injector) {
+      $rootScope = $injector.get('$rootScope');
+      $compile = $injector.get('$compile');
+    }));
+
+    beforeEach(function() {
+      element = '<mb-overlay text="' + text + '" hide-on="' + hideOn + '" show-on="' + showOn + '"></mb-overlay>';
+
+      $scope = $rootScope.$new();
+      element = $compile(element)($scope);
+      $scope.$digest();
+    });
+
+    var $templateCache;
+    beforeEach(inject(function ($injector) {
+      $templateCache = $injector.get('$templateCache');
+    }));
+
+    it('should contains a div with overlay class and the text inside it', function () {
+      expect(element.find('div').hasClass('overlay')).toBe(true);
+      expect(element.find('div').html()).toBe(text);
+    });
+
+    it('should be hide when hideOn event is broadcasted', function () {
+      $rootScope.$broadcast(hideOn);
+      expect(element.css('display')).toBe('none');
+    });
+
+    it('should be show when showOn event is broadcasted', function () {
+      $rootScope.$broadcast(hideOn);
+      expect(element.css('display')).toBe('none');
+      $rootScope.$broadcast(showOn);
+      expect(element.css('display')).not.toBe('none');
+    });
+  });
+});
