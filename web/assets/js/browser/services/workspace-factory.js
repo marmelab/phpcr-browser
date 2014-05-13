ecf609adad046c8e179b7482900f61fd5019788a
Robin Bressan <robin@buddey.net>
2014-05-13 16:14:40 +0200

    Add docblock for WorkspaceFactory

--- web/assets/js/browser/services/workspace-factory.js
+++ web/assets/js/browser/services/workspace-factory.js
@@ -9,33 +9,65 @@ define([
 
   app.factory('mbWorkspaceFactory', ['$q', 'mbApiFoundation', function($q, ApiFoundation) {
 
+    /**
+     * Workspace constructor
+     * @param {object} workspace
+     * @param {Repository} repository
+     * @param {Function} finder
+     */
     var Workspace = function(workspace, repository, finder) {
       this._restangular = workspace;
       this._repository = repository;
       this._finder = finder;
     };
 
+    /**
+     * Get the name of the workspace
+     * @return {string}
+     */
     Workspace.prototype.getName = function() {
       return this._restangular.name;
     };
 
+    /**
+     * Get the repository of the workspace
+     * @return {Repository}
+     */
     Workspace.prototype.getRepository = function() {
       return this._repository;
     };
 
+    /**
+     * Get a node in the workspace
+     * @param  {string} path
+     * @param  {object} params
+     * @return {promise}
+     */
     Workspace.prototype.getNode = function(path, params) {
       if (!path) { path = '/'; }
       return this._finder('/' + this.getRepository().getName() + '/' + this.getName() + path, params);
     };
 
+    /**
+     * Get the slug of the workspace
+     * @return {string}
+     */
     Workspace.prototype.getSlug = function() {
       return this.getName().replace(' ', '_');
     };
 
+    /**
+     * Delete this workspace
+     * @return {promise}
+     */
     Workspace.prototype.delete = function() {
       return ApiFoundation.deleteWorkspace(this.getRepository().getName(), this.getName());
     };
 
+    /**
+     * Create this workspace
+     * @return {promise}
+     */
     Workspace.prototype.create = function() {
       if (/^[a-zA-z][0-9a-zA-z]*$/.test(this.getName())) {
         return ApiFoundation.createWorkspace(this.getRepository().getName(), this.getName());
@@ -47,9 +79,23 @@ define([
     };
 
     return {
+
+      /**
+       * Build a Workspace object
+       * @param  {object} workspace
+       * @param  {Repository} repository
+       * @param  {Function} finder
+       * @return {Workspace}
+       */
       build: function(workspace, repository, finder) {
         return new Workspace(workspace, repository, finder);
       },
+
+      /**
+       * Test raw data validity
+       * @param  {object} data
+       * @return {boolean}
+       */
       accept: function(data) {
         return data.name !== undefined;
       }
