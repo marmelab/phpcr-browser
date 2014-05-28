9d6079250af074c16c52bc73130daeb8ebb7f9bb
Robin Bressan <robin@buddey.net>
2014-05-28 14:34:33 +0200

    Add validators

---
+++ web/assets/js/browser/services/validators/binary.js
@@ -0,0 +1,15 @@
+/* global define */
+/* jshint indent:2 */
+
+define([], function() {
+  'use strict';
+
+  return {
+    valid: function() {
+      return true;
+    },
+    override: function(value) {
+      return value;
+    }
+  };
+});
